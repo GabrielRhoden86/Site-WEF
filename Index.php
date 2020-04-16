281278a758bbef9ec3794ec150a9ae3b3feb3dff
Gabriel Rhoden <gabrielrhodden@gmail.com>
2020-04-16 10:06:57 -0300

    Inserida as meta tags

---
+++ Index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+ <html lang="pt-br">
+  
+  <head>
+  <title> WEF Veículos, venda de veículos </title>
+  <meta name = "description" content ="A WEF veículos e uma empresa especializada em anúncios de automóveis e motos na região de Curitiba. Contamos com milhares de veículos da sua preferência." >
+  <!-- a descrição deve ter no maximo 160 caracteres para que esteja visivel seu conteúdo quando a pesquisa no google for realizada-->
+
+  <meta charset = "UTF-8">
+
+  <meta name ="keywords" content = "WEF veículos,empresa de veículos em Curitiba, anuncios de carros, veículos Curitiba">
+  <!--A busca meta Keywords não é tão relevante para o google e sim para o outros navegadores-->
+
+  <link rel="icon" href="img\LogoSite.png" sizes="32x32">
+
+  <meta name ="author" content = "Gabriel Rhoden">
+
+  <meta name = "viewport"  content = "width=device-width, initial-scale=1">
+
+  
+  </head>
+ 
+  <body>
+   <h1>WEF Veículos</h1>
+  </body>
+
+ </html>
