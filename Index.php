1465014c458bd24787738bd5abe495a8c540fd6e
Gabriel Rhoden <gabrielrhodden@gmail.com>
2020-04-18 18:37:22 -0300

    Inserindo css

--- Index.php
+++ Index.php
@@ -5,23 +5,39 @@
   <title> WEF Veículos, venda de veículos </title>
   <meta name = "description" content ="A WEF veículos e uma empresa especializada em anúncios de automóveis e motos na região de Curitiba. Contamos com milhares de veículos da sua preferência." >
   <!-- a descrição deve ter no maximo 160 caracteres para que esteja visivel seu conteúdo quando a pesquisa no google for realizada-->
-
   <meta charset = "UTF-8">
-
   <meta name ="keywords" content = "WEF veículos,empresa de veículos em Curitiba, anuncios de carros, veículos Curitiba">
   <!--A busca meta Keywords não é tão relevante para o google e sim para o outros navegadores-->
-
   <link rel="icon" href="img\LogoSite.png" sizes="32x32">
-
+  <link rel ="stylesheet"  href ="libraries/SiteStyle.css">
   <meta name ="author" content = "Gabriel Rhoden">
-
   <meta name = "viewport"  content = "width=device-width, initial-scale=1">
-
   
   </head>
  
+
   <body>
-   <h1>WEF Veículos</h1>
+
+
+   <header> 
+    <nav>
+     <ul>
+      <li>Home</li>
+      <li>Link</li>
+     </ul>
+    </nav>
+   </header> 
+
+   <main>
+   </main>
+
+   <footer>
+   </footer>
+
+
+
+
+
   </body>
 
  </html>
